fd53f4603b8d89550a0fc6cb6e9cad281c80db86
xushihao <645479367@qq.com>
2024-06-05 14:47:33 +0800

    first commit

---
+++ src/main/java/com/hyf/chatwithgpt/ChatWithGptApplication.java
@@ -0,0 +1,16 @@
+package com.hyf.chatwithgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ */
+@SpringBootApplication
+public class ChatWithGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWithGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyf/chatwithgpt/ChatWithGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyf.chatwithgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWithGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
